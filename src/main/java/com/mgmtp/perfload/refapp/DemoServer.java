abb88986bbc07e2d787f817410bd128dce8ae12d
Reinhard Nägele <reinhard.naegele@mgm-tp.com>
2013-05-15 11:37:32 +0200

    Add license files and headers

--- src/main/java/com/mgmtp/perfload/refapp/DemoServer.java
+++ src/main/java/com/mgmtp/perfload/refapp/DemoServer.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2013 mgm technology partners GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.mgmtp.perfload.refapp;
 
 import org.eclipse.jetty.server.Server;
@@ -6,12 +21,10 @@ import org.eclipse.jetty.servlet.ServletContextHandler;
 
 import com.google.inject.servlet.GuiceFilter;
 
-public class DemoServer
-{
+public class DemoServer {
 	private static int PORT = 8080;
 
-	public static void main(final String[] args) throws Exception
-	{
+	public static void main(final String[] args) throws Exception {
 		int i = 0;
 		while (i < args.length && args[i].startsWith("-")) {
 			String arg = args[i++];
